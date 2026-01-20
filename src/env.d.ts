f0dff8b052777e30e922db99927e05138e504ed1
Johnny Gérard <john@jgerard.dev>
2026-01-20 15:46:55 +0100

    fix: @typescript-eslint/no-empty-object-type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,10 @@
+/**
+ * Code generated from `npm create cloudflare@latest -- my-astro-app --framework=astro`
+ * @see https://developers.cloudflare.com/workers/framework-guides/web-apps/astro/
+ */
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Locals extends Runtime {}
 }
