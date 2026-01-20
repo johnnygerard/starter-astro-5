65d633cf4930fc86e4120df5ffc5fa37876d01cf
Johnny Gérard <john@jgerard.dev>
2026-01-20 15:53:08 +0100

    build: allow importing Astro components in TS files

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,6 @@
+// Allow importing Astro components in TypeScript files without causing errors.
+declare module "*.astro";
+
 /**
  * Code generated from `npm create cloudflare@latest -- my-astro-app --framework=astro`
  * @see https://developers.cloudflare.com/workers/framework-guides/web-apps/astro/
